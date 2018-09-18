3735702fb15560d0677af33a0dd44e3f7bc0371f
benoit <benoit.delville@gmail.com>
2018-09-18 23:32:52 +1200

    prepare images

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,21 +4,20 @@ import PropTypes from 'prop-types'
 const Header = (props) => (
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
         <div className="logo">
-            <span className="icon fa-diamond"></span>
+            <span className="icon fa-shield"></span>
         </div>
         <div className="content">
             <div className="inner">
                 <h1>Buhurt Nortshore</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <p>Discover Buhurt in Northsore and in New Zealand</p>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('buhurt')}}>The sport</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('world')}}>In the World</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('nz')}}>In New Zealand</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('northshore')}}>In NorthShore</a></li>
             </ul>
         </nav>
     </header>
