2c69327dc1515e261c866731d7f25f711ce55c7d
benoit <benoit.delville@gmail.com>
2018-09-18 22:13:38 +1200

    set-up dimension

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('/media/data/workspace/web/northshore-buhurt/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ .cache/async-requires.js
@@ -0,0 +1,12 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/media/data/workspace/web/northshore-buhurt/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("/media/data/workspace/web/northshore-buhurt/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-index-js": () => import("/media/data/workspace/web/northshore-buhurt/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-page-2-js": () => import("/media/data/workspace/web/northshore-buhurt/src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */)
+}
+
+exports.data = () => import("/media/data/workspace/web/northshore-buhurt/.cache/data.json")
+
