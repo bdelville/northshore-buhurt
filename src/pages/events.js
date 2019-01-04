58e672a31f156d2e47eed28f875bf2465ab02d07
benoit <benoit.delville@gmail.com>
2019-01-04 22:21:30 +1300

    Only 1 month history

--- src/pages/events.js
+++ src/pages/events.js
@@ -7,9 +7,9 @@ class EventPage extends React.PureComponent {
   render() {
     return (
     <div>
-          <h2 className="major">Recent and upcoming Tournaments</h2>
+          <h2 className="major">Upcoming Tourney</h2>
           <p align="center">
-          <iframe src="https://hithredin.my.opendatasoft.com/explore/embed/dataset/bohurt-events/map/?sort=-date&q=date%3E%23now(months%3D-3)&location=3,-30,154&static=false&datasetcard=false&scrollWheelZoom=false" 
+          <iframe src="https://hithredin.my.opendatasoft.com/explore/embed/dataset/bohurt-events/map/?sort=-date&q=date%3E%23now(months%3D-1)&location=3,-30,154&static=false&datasetcard=false&scrollWheelZoom=false" 
             width="450" height="550" frameborder="0">
           </iframe>
           </p>
