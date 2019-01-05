54d1440e41b18c8780c96473aad271c387cfbfba
benoit <benoit.delville@gmail.com>
2019-01-05 14:12:47 +1300

    Map width is proportionnal

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,18 +1,36 @@
 import React from 'react'
 
-import hmbLogo from '../images/hmb-logo.png'
-import imcfLogo from '../images/imcf.png'
+class EventPage extends React.Component {
 
-class EventPage extends React.PureComponent {
+    constructor() {
+        super();
+        this.state = {
+            mapWidth: 450,
+            mapHeight: 550
+        }
+    }
+
+  componentDidUpdate(prevProps, prevState) {
+    const articleContainer = document.getElementById('main');
+    const mapWidth = articleContainer.clientWidth - 70;
+    if (this.state.mapWidth !== mapWidth) {
+      this.setState({ mapWidth });
+      console.log(articleContainer.clientHeight);
+    }
+  }
+
   render() {
     return (
     <div>
-          <h2 className="major">Upcoming Tourney</h2>
+          <h2 className="major">Upcoming Tourneys</h2>
           <p align="center">
           <iframe src="https://hithredin.my.opendatasoft.com/explore/embed/dataset/bohurt-events/map/?sort=-date&q=date%3E%23now(months%3D-1)&location=4,-36,166&static=false&datasetcard=false&scrollWheelZoom=false" 
-            width="450" height="550" frameborder="0">
+            width={this.state.mapWidth} height="550" frameborder="0" title="Open Data Soft events map">
           </iframe>
           </p>
+
+          <p><i>The map of events is self maintained, as part of the Buhurt App project. 
+          It might not be accurate and the data is for information only</i></p>
     </div>
     )
   }
