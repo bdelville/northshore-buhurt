633525fde8b72bac4560e6c74c29418e06c3bf0b
benoit <benoit.delville@gmail.com>
2019-11-14 18:25:51 +1300

    Quick legal fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,8 @@ const Header = (props) => (
         <nav>
             <ul>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('buhurt')}}>The sport</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('world')}}>In the World</a></li>
+                <!-- TODO Restore once split in 2 tabs -->
+                <!--<li><a href="javascript:;" onClick={() => {props.onOpenArticle('world')}}>In the World</a></li>-->
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('nz')}}>New Zealand</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('help')}}>Get Started</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('events')}}>Where to go?</a></li>
